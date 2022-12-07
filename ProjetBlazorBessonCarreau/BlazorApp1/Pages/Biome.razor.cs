4b3c59a217517bdebb908c9b9f5dee4682094786
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2022-12-07 10:21:53 +0100

    ajout projet

---
+++ ProjetBlazorBessonCarreau/BlazorApp1/Pages/Biome.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using BlazorApp1.Models;
+
+namespace BlazorApp1.Pages
+{
+    public partial class Biome
+    {
+        private BiomeItem[] biomes;
+
+        [Inject]
+        public HttpClient Http { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+
+            biomes = await Http.GetFromJsonAsync<BiomeItem[]>($"{NavigationManager.BaseUri}biome-fake-data.json");
+        }
+    }
+}
