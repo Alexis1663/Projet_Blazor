78baf4c16c868537d1b498155db015401648f5ff
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2022-12-10 08:20:00 +0100

    ajout correctif

--- ProjetBlazorBessonCarreau/BlazorApp1/Program.cs
+++ ProjetBlazorBessonCarreau/BlazorApp1/Program.cs
@@ -6,8 +6,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-//builder.Services.AddSingleton<BiomeService>();
-
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
