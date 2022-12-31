4b051bf9b812f17a96176d937f7a18ad7cb27b95
Alexis1663 <92861778+Alexis1663@users.noreply.github.com>
2022-12-31 19:16:43 +0100

    lien des 2 codes

--- ProjetBlazorBessonCarreau/BlazorApp1/Components/Mob.razor.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace BlazorApp1.Components
-{
-    public partial class Mob
-    {
-    }
-}
--- ProjetBlazorBessonCarreau/BlazorApp1/Models/BiomeItem.cs
+++ ProjetBlazorBessonCarreau/BlazorApp1/Models/BiomeItem.cs
@@ -2,6 +2,7 @@
 {
     public class BiomeItem
     {
+        public int Id { get; set; }
         public enum Env { Hostile, Paysible }
 
         public String Nom { get; set; }
@@ -11,5 +12,7 @@
         public Env Environemment { get; set; }
 
         public String Image { get; set; }
+
+        public List<MobClass> Mobs { get; set; }
     }
 }
