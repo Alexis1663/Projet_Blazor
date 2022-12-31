d8826be1dbeb7da59bfcc8b05de9351eb150d986
Misticku <bessonjeremy.pro@gmail.com>
2022-12-31 12:36:41 +0100

    Modification des biomes et ajouts de leur données dans le localStorage pour fonctionner dans la gestion de mobs. Cependant, un petit bug à régler pour récupérer les mobs présents dans le biome

--- ProjetBlazorBessonCarreau/BlazorApp1/Components/BiomeMobs.razor.cs
+++ ProjetBlazorBessonCarreau/BlazorApp1/Components/BiomeMobs.razor.cs
@@ -1,4 +1,5 @@
 ﻿using BlazorApp1.Models;
+using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Collections.ObjectModel;
@@ -9,9 +10,9 @@ namespace BlazorApp1.Components
 {
     public partial class BiomeMobs
     {
-        //Identifiant du biome choisi
+        //Identifiant du biome choisi, il est récupéré lorsque l'utilisateur clique sur un biome pour en connaître les mobs
         [Parameter]
-        public int Id { get; set; }
+        public int IdBiome { get; set; }
 
         [Inject]
         public HttpClient Http { get; set; }
@@ -19,16 +20,21 @@ namespace BlazorApp1.Components
         [Inject]
         public NavigationManager NavigationManager { get; set; }
 
-
+        //Le mob actuellement pris par la drag and drop
         public MobClass CurrentDragItem { get; set; }
 
         [Parameter]
         //Monstres présents dans le biome
         public List<MobClass> Items { get; set; }
 
+        //Tableau des biomes 
+        public BiomeItem[] Biomes { get; set; }
 
         //Monstres disponibles
         public List<MobClass> AvailableMobs { get; set; }
+
+        [Inject]
+        public ILocalStorageService LocalStorage { get; set; }
 
 
         [Parameter]
@@ -40,17 +46,20 @@ namespace BlazorApp1.Components
         [Inject]
         internal IJSRuntime JavaScriptRuntime { get; set; }
 
-        protected override async Task OnInitializedAsync()
+        protected override async Task OnAfterRenderAsync(bool firstrender)
         {
-            BiomeItem[] biomes = (await Http.GetFromJsonAsync<BiomeItem[]>($"{NavigationManager.BaseUri}biome-fake-data.json"));
+            //En essayant d'utiliser OnInitializedAsync ici, il y a un bug en essayant de récupréer les données de biome, mais étrangement, il ne se manifeste que si la ligne avec "Items" n'est pas commentée
+            //Cependant,  en utilisant OnAfterRenderAsync, aucune données ne s'affiche bien que le bug ne se présente pas (même les mobs dispos ne sont pas montrés)
+            Biomes = await LocalStorage.GetItemAsync<BiomeItem[]>("dataBiome");
             AvailableMobs = (await Http.GetFromJsonAsync<MobClass[]>($"{NavigationManager.BaseUri}AvailableMobs.json")).ToList();
-            Items = biomes[Id].Mobs;
+            Items = Biomes[IdBiome].Mobs;
         }
 
 
 
         public void AddMob(MobClass mob)
         {
+            //créer une méthode pour ajouter le mob dans le biome (peut être avec la DataService)
             Items.Add(mob);
             StateHasChanged();
         }
